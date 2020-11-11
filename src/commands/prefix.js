e5e82cf8bffac8e417943219a27acaddf708a067
pierre <pierre.nadin@mic-belgique.be>
2020-11-11 14:56:40 +0100

    Change status when changing prefix

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -12,7 +12,8 @@ module.exports = async (msg, args) => {
             .setColor(0xff0000)
             .setDescription('Nouveau prefix: ' + prefix[0]);
         await msg.channel.send(embed);
-        msg.guild.members.fetch(process.env.BOT_ID).then(setPresence({ status: "idle", activity: { name: prefix[0] + 'help | v1.0' } }));
+        var client = await msg.guild.members.fetch(process.env.BOT_ID);
+        client.user.setPresence({ status: "idle", activity: { name: prefix[0] + 'help | v1.0' } })
     } else {
         const embed = new Discord.MessageEmbed()
             .setTitle('Prefix')
